5b9119a8aba7de50ce38431ba7282ac3ee02865b
Joe Sellner <joe@crwd.local>
2022-05-20 16:29:15 -0500

    ported to python 2.7

--- connector_send_rcv.py
+++ connector_send_rcv.py
@@ -15,6 +15,10 @@ import time
 import threading
 
 side = sys.argv[1]
+try:
+    print(sys.argv[2])
+except:
+    pass
 opponent = ""
 data = []
 HOST = "127.0.0.1"
@@ -23,7 +27,7 @@ udpPort = udp_ports[side]
 #PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 state = "disconnected"
 jitterBuff = 0.04
-udp = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 udp.bind((HOST, udpPort))
 sync_delay = 0
 start = 0
@@ -31,51 +35,50 @@ start = 0
 # with socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM) as udp:
 #     udp.bind((HOST, udpPort))
 
-def initConnection(side):
+def initConnection(ms):
     global opponent
     global udpPort
     global start
     ip = requests.get('https://api.ipify.org', verify=False).content.decode('utf8')
     #print(f'My IP address is: {ip}')
-    if side == "slave":
+    if ms == "slave":
         print("I'm slave")
         PORT = tcpPort
         #udpPort = udp_ports['slave']
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcp:
-            tcp.bind((HOST, PORT))
-            tcp.listen()
-            conn, addr = tcp.accept()
-            with conn:
-                while True:
-                    data = conn.recv(1024)
-                    if data.split(b'ip')[0] == b'ready':
-                        conn.sendall(b'g2gip'+str.encode(ip))
-                        opponent = data.split(b'ip')[1].decode('utf-8')
-                        print("Ready for Netlink!")
-                        print(f'Opponent IP: {opponent}')
-                        ts = time.time()
-                        start = ts + 0.2 
-                        conn.sendall(struct.pack('d',ts))
-                        return "connected"
-                    if not data:
-                        break
-    if side == "master":
+        tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tcp.bind((HOST, PORT))
+        tcp.listen(5)
+        conn, addr = tcp.accept()
+        while True:
+            data = conn.recv(1024)
+            if data.split(b'ip')[0] == b'ready':
+                conn.sendall(b'g2gip'+(ip))
+                opponent = data.split(b'ip')[1].decode('utf-8')
+                print("Ready for Netlink!")
+                print('Opponent IP: %s'% opponent)
+                ts = time.time()
+                start = ts + 0.2 
+                conn.sendall(struct.pack('d',ts))
+                return "connected"
+            if not data:
+                break
+    if ms == "master":
         print("I'm master")
         PORT = tcpPort
         #udpPort = udp_ports['master']
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcp:
-            tcp.connect((HOST, PORT))
-            tcp.sendall(b"readyip"+str.encode(ip))
-            data = tcp.recv(1024)
-            if data.split(b'ip')[0] == b'g2g':
-                opponent = data.split(b'ip')[1].decode('utf-8')
-                print("Ready for Netlink!")
-                print(f'Opponent IP: {opponent}')
-                ts = tcp.recv(1024)
-                st = struct.unpack('d',ts)[0]
-                delay = st+0.2-time.time()
-                start = delay + time.time()
-                return "connected"
+        tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tcp.connect((HOST, PORT))
+        tcp.sendall(b"readyip"+(ip))
+        data = tcp.recv(1024)
+        if data.split(b'ip')[0] == b'g2g':
+            opponent = data.split(b'ip')[1].decode('utf-8')
+            print("Ready for Netlink!")
+            print('Opponent IP: %s'% opponent)
+            ts = tcp.recv(1024)
+            st = struct.unpack('d',ts)[0]
+            delay = st+0.2-time.time()
+            start = delay + time.time()
+            return "connected"
                 
     else:
         return "error"
@@ -115,7 +118,7 @@ def printer():
                     first_run = False
                 #print(toSend)
                 latency = round(((time.time() - ts)*1000),0)
-                print(latency)
+                print('latency: %sms' % latency)
                 time.sleep(0.02)
             except:
                 #time.sleep(.01)
@@ -129,9 +132,11 @@ def sender():
         oppPort = udp_ports['slave']
     while(state == "connected"):
         raw_input = side
-        delimiter = str.encode("sequenceno",'ANSI')
+        # delimiter = str.encode("sequenceno",'ANSI')
+        delimiter = "sequenceno"
         try:
-            payload = str.encode(raw_input,'ANSI')
+            # payload = str.encode(raw_input,'ANSI')
+            payload = raw_input
             ts = time.time()
             udp.sendto((payload+delimiter+struct.pack('d',ts)), (HOST,oppPort))
             
@@ -142,9 +147,12 @@ def sender():
             
 
                 
-t1 = threading.Thread(target=listener, daemon=True)
-t2 = threading.Thread(target=printer, daemon=True)
-t3 = threading.Thread(target=sender, daemon=True)
+t1 = threading.Thread(target=listener)
+t1.setDaemon(True)
+t2 = threading.Thread(target=printer)
+t2.setDaemon(True)
+t3 = threading.Thread(target=sender)
+t3.setDaemon(True)
 state = initConnection(side)
 print(state)
 sync_delay = start-time.time()
